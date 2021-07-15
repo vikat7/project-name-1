d457ed7320cb414a27f999e6f5fc0feed025b765
RPOCTHblN <rageplay@mail.ru>
2021-07-15 19:56:59 +0700

    My bad try to start work on java

---
+++ src/app.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Паш, мы почти смогли, или не очень';
+  }
+  getWorkTime(useridParam: number, dateParam: string /* string в формате 'YYYY-MM-DD' */): number {
+    var z=0;
+    axios.get(`http://admin:thuvj1sh@dev-redmine.gnedov.info:8380/time_entries.json?user_id=${useridParam}&spent_on=${dateParam}`).then(function (response) {
+    
+      var timeEntries = response.data.time_entries;
+      for (var i = 1; i < timeEntries.length; i++) {
+        var timeItem = timeEntries[i];
+        var user = timeItem.user;
+        var userid = user.id;
+        var hours = timeItem.hours;
+        var Date = timeItem.spent_on;
+        console.log(i, ':', 'userid =', userid, '; Hours =', hours, '; Date =', Date);
+        z=z+timeItem.hours;
+        console.log(z);
+      }
+    })
+    return z;
+  }
+}
+
+
