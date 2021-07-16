03a230d5607b7250dcd789692fb577b26193d0cb
pavel.gnedov <pavel.gnedov@eltex.loc>
2021-07-16 20:18:42 +0700

    Добавил post-функцию get-work-time

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequestType } from './request-type';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Post('get-work-time')
+  async getWorkTime(@Body() request: RequestType): Promise<string> {
+    return 'TODO: get-work-time';
   }
 }
---
+++ src/request-type.ts
@@ -0,0 +1,4 @@
+export type RequestType = {
+	date: string,
+	userId: number
+};
